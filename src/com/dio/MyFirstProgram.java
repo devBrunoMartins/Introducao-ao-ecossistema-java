6da676ccf3fb4ba8129d7fe21af4546199687d31
Bruno martins <bruno.eletr@gmail.com>
2021-12-05 14:11:41 -0300

    Altera a classe Order.java e implementa a documentação

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -2,9 +2,11 @@ package com.dio;
 
 import com.dio.base.Order;
 
+import java.math.BigDecimal;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
-        final Order order = new Order("code1234");
+        final Order order = new Order("code1234", new BigDecimal("101.00"));
         System.out.println(order);
     }
 }
